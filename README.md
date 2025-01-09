76aa154a66167f3084758b28f2d89fac1bc5c4bb
Sotatek-TungBui4 <tung.bui4@sotatek.com>
2025-01-09 17:28:25 +0700

    chore: update doc

--- README.md
+++ README.md
@@ -19,3 +19,51 @@
 - [Crawler & GraphQL Service](crawler)
 - [Telegram Notification Service](notification_service)
 
+
+## How to run
+
+1. Set up services
+
+- Go to [crawler](crawler)
+
+```sh
+npm install
+docker compose up -d
+npx hardhat node
+```
+
+> After you start node by hardhat, there's a list of private keys, use 1 of them replace to `multisig-sc/clients/index.ts:14`.
+
+2. Start service
+
+- Start [notification_service](notification_service)
+
+```sh
+npm run start:dev
+```
+
+- Deploy smart contracts. Go to [multisig-sc](multisig-sc)
+
+```sh
+npm install
+bash deploy.sh
+```
+
+then use MultiSig address in `multisig-sc/ignition/deployments/chain-31337/deployed_addresses.json:4` replace in `crawler/ponder.config.ts:32`.
+
+- Start [crawler](crawler)
+
+```sh
+npm install
+npm run dev
+```
+
+- Verify it work
+
+Go to [multisig-sc](multisig-sc), and run:
+
+```sh
+npx ts-node multisig-sc/clients/index.ts
+```
+
+Now check the terminal 👍.
--- multisig-sc/clients/index.ts
+++ multisig-sc/clients/index.ts
@@ -49,13 +49,13 @@ async function faucet() {
 }
 
 async function main() {
-    // // faucet
-    // await faucet();
+    // faucet
+    await faucet();
     
     // give the agent wallet#3, so client use only 2 first wallets 
     const wallet0MultiSig = MultiSig__factory.connect(DeployedAddress['MultiSig#MultiSig'], wallets[0]);
     const wallet1MultiSig = MultiSig__factory.connect(DeployedAddress['MultiSig#MultiSig'], wallets[1]);
-    const agentTxIndex = 2;
+    const agentTxIndex = 0;
 
     const mockERC20 = MockERC20__factory.connect(DeployedAddress['MockERC20#MockERC20'], wallets[1]);
     const mockERC721 = MockERC721__factory.connect(DeployedAddress['MockERC721#MockERC721'], wallets[1]);
@@ -64,32 +64,32 @@ async function main() {
 
     const encodedMintERC20 = distributorInterface.encodeFunctionData('mintERC20', [wallets[2].address, 1000000000])
     const encodedMintERC721 = distributorInterface.encodeFunctionData('mintERC721', [wallets[2].address, 0, "https://example.com/"])
-    // const wallet0SubmitTxMintERC20 = await (
-    //     await wallet0MultiSig.submitTransaction(DeployedAddress['Distributor#Distributor'], 0, encodedMintERC20)
-    // ).wait(1);
-    // console.info("submitted mint ERC20 transaction")
+    const wallet0SubmitTxMintERC20 = await (
+        await wallet0MultiSig.submitTransaction(DeployedAddress['Distributor#Distributor'], 0, encodedMintERC20)
+    ).wait(1);
+    console.info("submitted mint ERC20 transaction")
     // const wallet0SubmitTxMintERC721 = await (
     //     await wallet0MultiSig.submitTransaction(DeployedAddress['Distributor#Distributor'], 0, encodedMintERC721)
     // ).wait(1);
     // console.info("submitted mint ERC721 transaction")
 
-    // await (
-    //     await wallet0MultiSig.confirmTransaction(agentTxIndex)
-    // ).wait(1);
-    // console.info("Wallet #1 confirmed");
-    // await (
-    //     await wallet1MultiSig.confirmTransaction(agentTxIndex)
-    // ).wait(1);
-    // console.info("Wallet #2 confirmed");
-    // // wallet#1 & wallet#2 confirmed, => 2/3 wallets have confirmed => agent can execute transaction
+    await (
+        await wallet0MultiSig.confirmTransaction(agentTxIndex)
+    ).wait(1);
+    console.info("Wallet #1 confirmed");
+    await (
+        await wallet1MultiSig.confirmTransaction(agentTxIndex)
+    ).wait(1);
+    console.info("Wallet #2 confirmed");
+    // wallet#1 & wallet#2 confirmed, => 2/3 wallets have confirmed => agent can execute transaction
 
-    // // execute transaction
-    // await (
-    //     await wallet1MultiSig.executeTransaction(agentTxIndex)
-    // ).wait(1);
-    // console.info(`Tx at ${agentTxIndex} executed`);
+    // execute transaction
+    await (
+        await wallet1MultiSig.executeTransaction(agentTxIndex)
+    ).wait(1);
+    console.info(`Tx at ${agentTxIndex} executed`);
 
-    // console.log(await mockERC20.balanceOf(wallets[2].address))
+    console.log(await mockERC20.balanceOf(wallets[2].address))
     // console.log(await mockERC721.balanceOf(wallets[2].address))
 
 }
